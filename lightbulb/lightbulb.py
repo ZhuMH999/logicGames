dce278e475b80c2427f6bedf3aa1c6f6a4d373c2
Ryan <zhumh.ryan@gmail.com>
2025-06-14 20:39:14 +0800

    new features: lightbulb game code done

--- lightbulb/lightbulb.py
+++ lightbulb/lightbulb.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 
-WIDTH, HEIGHT = 600, 800
+WIDTH, HEIGHT = 600, 600
+SIDES = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
 class View:
     def __init__(self, model, win):
@@ -9,6 +11,17 @@ class View:
 
     def draw(self):
         self.win.fill((100, 100, 100))
+
+        for i in range(3):
+            for j in range(3):
+                pygame.draw.rect(self.win, (0, 0, 0), (120+i*120, 120+j*120, 124, 124), 4)
+                if self.model.lights[j][i] == 0:
+                    pygame.draw.rect(self.win, (60, 60, 60), (124+i*120, 124+j*120, 116, 116))
+                if self.model.lights[j][i] == 1:
+                    pygame.draw.rect(self.win, (180, 180, 0), (124+i*120, 124+j*120, 116, 116))
+
+        if self.model.won:
+            print('you winnnnn')
 
     def get_text_widget_and_center(self, rgb, c_x, c_y, font, text):
         widget = font.render(text, True, rgb)
@@ -19,6 +32,37 @@ class View:
 class Model:
     def __init__(self):
         self.run = True
+        self.lights = [[random.randint(0, 1) for _ in range(10)] for _ in range(10)]
+        self.won = False
+
+    def get_mouse_pos(self, x, y):
+        if 120 < x < 480 and 120 < y < 480 and self.won == False:
+            xpos = x // 120 - 1
+            ypos = y // 120 - 1
+
+            if self.lights[ypos][xpos] == 0:
+                self.lights[ypos][xpos] = 1
+            else:
+                self.lights[ypos][xpos] = 0
+
+            for i in range(4):
+                try:
+                    if self.lights[ypos + SIDES[i][1]][xpos + SIDES[i][0]] == 0:
+                        self.lights[ypos + SIDES[i][1]][xpos + SIDES[i][0]] = 1
+                    else:
+                        self.lights[ypos+ SIDES[i][1]][xpos + SIDES[i][0]] = 0
+
+                except IndexError:
+                    continue
+
+        self.won = self.check_for_win()
+
+    def check_for_win(self):
+        for i in range(3):
+            for j in range(3):
+                if self.lights[j][i] != 1:
+                    return False
+        return True
 
 class Controller:
     def __init__(self):
@@ -37,6 +81,9 @@ class Controller:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.model.run = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = pygame.mouse.get_pos()
+                    self.model.get_mouse_pos(x, y)
 
             self.view.draw()
 
